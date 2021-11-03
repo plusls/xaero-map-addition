30c4d483432141a7832862c207286d8fd3e1063d
plulsls <plusls@qq.com>
2021-11-03 20:11:31 +0800

    init

---
+++ src/main/java/com/plusls/xma/ModInfo.java
@@ -0,0 +1,24 @@
+package com.plusls.xma;
+
+import net.fabricmc.loader.api.FabricLoader;
+import net.fabricmc.loader.api.ModContainer;
+import net.minecraft.util.Identifier;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.Optional;
+
+public class ModInfo {
+    public static String MOD_ID = "xaero_map_addition";
+    public static String MOD_VERSION;
+    public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
+
+    static {
+        Optional<ModContainer> modContainerOptional = FabricLoader.getInstance().getModContainer(MOD_ID);
+        modContainerOptional.ifPresent(modContainer -> MOD_VERSION = modContainer.getMetadata().getVersion().getFriendlyString());
+    }
+
+    public static Identifier id(String path) {
+        return new Identifier(MOD_ID, path);
+    }
+}
---
+++ src/main/java/com/plusls/xma/XaeroMapAddition.java
@@ -0,0 +1,11 @@
+package com.plusls.xma;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class XaeroMapAddition implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+        ModInfo.LOGGER.info("xma loaded.");
+    }
+}
